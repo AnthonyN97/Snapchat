0e2a41a62b6969d05aa5cab54896995c7e2bc46e
Karlo <karlo.carigga@tecsup.edu.pe>
2022-10-31 15:32:52 -0500

    finalizado

---
+++ NovaSnapchat/iniciarSesionViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  NovaSnapchat
+//
+//  Created by Mac 16 on 31/10/22.
+//
+
+import UIKit
+import FirebaseAuth
+
+class iniciarSesionViewController: UIViewController {
+
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBAction func iniciarSesionTapped(_ sender: Any) {
+        Auth.auth().signIn(withEmail: emailTextField.text!, password: passwordTextField.text!) { (user, error) in print("Intentando Iniciar Sesion")
+            if error != nil{
+                print("Se presento el siguiente error: \(error)")
+            }else{
+                print("Inicio de sesion exitoso")
+            }
+        }
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
